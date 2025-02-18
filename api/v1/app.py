0a67eb88007c57cf0f7d9e268b8c7af09ac9923c
makau <makauwanyoike@gmail.com>
2025-02-18 23:15:13 +0300

    docstrings?

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""app"""
+"""principal application app"""
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
@@ -11,7 +11,7 @@ from flasgger import Swagger
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_storage(exception):
@@ -34,6 +34,7 @@ swagger = Swagger(app)
 
 
 if __name__ == "__main__":
+    """main"""
     host = getenv("HBNB_API_HOST", default="0.0.0.0")
     port = getenv("HBNB_API_PORT", default=5000)
     app.run(host, int(port), threaded=True)
